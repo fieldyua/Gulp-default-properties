12ae55d6f63c728dc393d2df53d2ff3ab425089d
Yevhenii <fieldyspz@gmail.com>
2017-11-20 12:58:54 +0200

    Initial commit.

---
+++ src/js/a.js
@@ -0,0 +1,10 @@
+export class A {
+    constructor(foo){
+        console.log(foo);
+    }
+
+    sayHello(){
+        console.log("Hello from A.");
+    }
+
+}
---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+import {A} from "./a";
+
+let a = new A('foo');
+a.sayHello();
+console.log('another log');
